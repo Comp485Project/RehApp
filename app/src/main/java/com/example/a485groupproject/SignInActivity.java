e9d5589fe4f9a16398d01a9ff200f9a56b442241
Chiamaka Nnawulezi <chiamakannawulezi@gmail.com>
2021-11-13 15:07:52 -0500

    Adding Parse backend

--- app/src/main/java/com/example/a485groupproject/SignInActivity.java
+++ app/src/main/java/com/example/a485groupproject/SignInActivity.java
@@ -1,4 +1,67 @@
 package com.example.a485groupproject;
 
-public class SignInActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class SignInActivity extends AppCompatActivity {
+
+    public static final String TAG = "SignInActivity";
+    private EditText username;
+    private EditText password;
+    private Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick signIn button");
+                String user_username = username.getText().toString();
+                String user_password = password.getText().toString();
+                signInUser(user_username, user_password);
+            }
+        });
+
+    }
+
+    private void signInUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user " + username);
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with login", e);
+                    return;
+                }
+                //TODO: navigate to the main activity if the user has signed in properly
+                goProfile();
+                Toast.makeText(SignInActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goProfile() {
+        Intent i = new Intent(this, UserProfileActivity.class);
+        startActivity(i);
+        finish();
+    }
+
 }
